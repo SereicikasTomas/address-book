e004de81216380b30ce7816b69bc22f7d9e372b2
jacando.tos <sereicikas.t@gmail.com>
2021-03-25 17:16:22 +0100

    test custom hook

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,9 +1,18 @@
 import { Fragment } from 'react';
+import useUserFetch from '../../api/hooks/useUserFetch';
+
+import { User } from '../../api/hooks/interfaces';
 
 function List(): JSX.Element {
+  const [loading, error, users, hasMore] = useUserFetch(1);
+  console.log(loading, error, users, hasMore);
   return (
     <Fragment>
-      <ul>List</ul>
+      <ul>
+        {users.map((user: User) => (
+          <li key={user.id}>{user.first}</li>
+        ))}
+      </ul>
     </Fragment>
   );
 }
