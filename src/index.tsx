72445d3953c80cc2bfa3737c4ceed3a70b475eaf
jacando.tos <sereicikas.t@gmail.com>
2021-03-25 14:31:49 +0100

    import global styles and themes

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
 import App from './components/App';
+import { lightTheme } from './global-styles/themes';
+import GlobalStyles from './global-styles/styles';
 
 function RootComponent() {
   return (
     <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider theme={lightTheme}>
+        <BrowserRouter>
+          <GlobalStyles />
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </React.StrictMode>
   );
 }
