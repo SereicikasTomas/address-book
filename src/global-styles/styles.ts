1169e4d347e4676b9879cc5f73e5ad45f854e958
jacando.tos <sereicikas.t@gmail.com>
2021-03-25 14:31:25 +0100

    add global styles

---
+++ src/global-styles/styles.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    box-sizing: border-box;
+    font-family: "Roboto", sans-serif;
+    font-weight: 300;
+    line-height: 1.7;
+    font-size: 1.6rem;
+    color: ${({ theme: { colorSecondary } }) => colorSecondary};
+    background-color: ${({ theme: { colorPrimary } }) => colorPrimary};
+  }
+
+  #root {
+    height: 100vh;
+    width: 100vw;
+  }
+`;
---
+++ src/global-styles/themes.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  colorPrimary: '#324B4C',
+  colorSecondary: '#F5F5F5',
+  accentColor: '#00C6CF',
+};
+
+export const lightTheme: DefaultTheme = {
+  colorPrimary: '#F5F5F5',
+  colorSecondary: '#324B4C',
+  accentColor: '#068488',
+};
