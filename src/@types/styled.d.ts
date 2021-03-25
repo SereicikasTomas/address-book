2860e1a5e6f6384e8b5b7f7e71d9185de5588983
jacando.tos <sereicikas.t@gmail.com>
2021-03-25 14:32:00 +0100

    add type for themes

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colorSecondary: string;
+    colorPrimary: string;
+    accentColor: string;
+  }
+}
