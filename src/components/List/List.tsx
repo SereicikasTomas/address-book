74e22fc88f8685c8a14524e9952c9639d5678486
jacando.tos <sereicikas.t@gmail.com>
2021-03-26 18:35:01 +0100

    add initial infinite load implementation

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,19 +1,39 @@
-import { Fragment } from 'react';
+import { Fragment, useRef, useState, useCallback } from 'react';
 import useUserFetch from 'api/hooks/useUserFetch';
 
 import Card from './Card';
 import { User } from 'types/interfaces';
 
+import * as Styled from './styles';
+
 function List(): JSX.Element {
-  const [loading, error, users, hasMore] = useUserFetch(1);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [loading, error, users, hasMore] = useUserFetch(pageNumber);
   console.log(loading, error, users, hasMore);
+
+  const observer = useRef<IntersectionObserver>();
+  const lastUserCardRef = useCallback(
+    (node) => {
+      console.log(node);
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) setPageNumber((prevState: number) => prevState + 1);
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore],
+  );
+
   return (
     <Fragment>
-      <ul>
+      <Styled.List>
         {users.map((user: User) => (
           <Card key={user.id} {...user} />
         ))}
-      </ul>
+      </Styled.List>
+      {loading && <div>Loading...</div>}
+      {hasMore && <div ref={lastUserCardRef}>Last</div>}
     </Fragment>
   );
 }
