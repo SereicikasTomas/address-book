a5a96117c6bfd1ef32de9893402c708492eab330
jacando.tos <sereicikas.t@gmail.com>
2021-03-29 22:07:43 +0200

    render values in cards

--- src/components/List/Card/Card.tsx
+++ src/components/List/Card/Card.tsx
@@ -1,8 +1,17 @@
-import { User } from 'types/interfaces';
+import { CardProps } from 'types/interfaces';
 
-function Card(props: User): JSX.Element {
-  const { first } = props;
-  return <li>{first}</li>;
+import * as Styled from './styles';
+
+function Card(props: CardProps): JSX.Element {
+  const { firstName, lastName, email, picture, index } = props;
+  return (
+    <Styled.Card delay={index}>
+      <img src={picture} alt="User picture" />
+      <p>{firstName}</p>
+      <p>{lastName}</p>
+      <h6>{email}</h6>
+    </Styled.Card>
+  );
 }
 
 export default Card;
