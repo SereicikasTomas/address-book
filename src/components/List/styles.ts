c7ef589d5c6a0229c32d9c0622602b86a7aab6f1
jacando.tos <sereicikas.t@gmail.com>
2021-03-29 22:07:57 +0200

    add initial list styles

---
+++ src/components/List/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 4rem;
+  justify-content: flex-start;
+  padding: 0 30rem;
+`;
