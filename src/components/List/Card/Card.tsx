7f3e726c821eb409a8df9edae86a0ae0c6c6c84b
jacando.tos <sereicikas.t@gmail.com>
2021-03-30 23:56:08 +0200

    add gradient and additional animations to cards

--- src/components/List/Card/Card.tsx
+++ src/components/List/Card/Card.tsx
@@ -3,13 +3,18 @@ import { CardProps } from 'types/interfaces';
 import * as Styled from './styles';
 
 function Card(props: CardProps): JSX.Element {
-  const { firstName, lastName, email, picture, index } = props;
+  const { firstName, lastName, email, picture, index, gradient } = props;
   return (
     <Styled.Card delay={index}>
-      <img src={picture} alt="User picture" />
-      <p>{firstName}</p>
-      <p>{lastName}</p>
-      <h6>{email}</h6>
+      <Styled.Background gradient={gradient}>
+        <Styled.Picture src={picture} alt="User picture" />
+      </Styled.Background>
+      <Styled.Information>
+        <p>
+          {firstName} {lastName}
+        </p>
+        <h6>{email}</h6>
+      </Styled.Information>
     </Styled.Card>
   );
 }
--- src/components/List/Card/styles.ts
+++ src/components/List/Card/styles.ts
@@ -1,10 +1,12 @@
 import styled, { keyframes } from 'styled-components';
 
 type CardType = { delay: number };
+type BackgroundType = { gradient: string };
 
 const appearAnimation = keyframes`
  0% {
-   transform: translateY(50px);
+  opacity: 0;
+  transform: translateY(-50px);
  }
  100% {
   transform: translateY(0px);
@@ -13,10 +15,53 @@ const appearAnimation = keyframes`
 `;
 
 export const Card = styled.li`
-  border: 1px solid black;
+  position: relative;
   flex: 0 1 calc(25% - 3rem);
   height: 30rem;
-  opacity: 0;
-  animation: ${appearAnimation} 1s forwards;
+  animation: ${appearAnimation} 800ms backwards;
   animation-delay: ${({ delay }: CardType) => (delay % 50) * 150 + 'ms'};
+  box-shadow: 0 1rem 2rem 0 rgba(0, 0, 0, 0.1);
+  transition: transform 500ms;
+  border-radius: 15px;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    opacity: 0;
+    box-shadow: 0 2rem 4rem 0 rgba(0, 0, 0, 0.3);
+    transition: opacity 500ms;
+    z-index: -1;
+    border-radius: 15px;
+  }
+
+  &:hover {
+    transform: scale(1.05) translateY(-10px);
+    &::after {
+      opacity: 1;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  background-image: linear-gradient(200deg, ${({ gradient }: BackgroundType) => gradient});
+  height: 10rem;
+  display: flex;
+  padding-left: 1rem;
+  border-radius: 15px;
+`;
+
+export const Picture = styled.img`
+  margin-top: auto;
+  border-radius: 50%;
+  border: 2px solid ${({ theme: { colorPrimary } }) => colorPrimary};
+  transform: translateY(50%);
+  border-radius: 15px;
+`;
+
+export const Information = styled.div`
+  padding: 3rem 1rem 2rem;
 `;
