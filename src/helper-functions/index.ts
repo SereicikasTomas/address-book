9585b89ea68c442120684bc697b07af2d5d237be
jacando.tos <sereicikas.t@gmail.com>
2021-03-30 23:55:01 +0200

    add helper function to generate color gradients

---
+++ src/helper-functions/index.ts
@@ -0,0 +1,12 @@
+export const generateGradient = (): string => {
+  const gradients = [
+    'rgb(43, 77, 130), rgb(40, 144, 172)',
+    'rgb(140, 85, 250), rgb(143, 230, 243)',
+    'rgb(40, 23, 76),rgb(232, 90, 94)',
+    'rgb(84, 21, 156),rgb(132, 51, 77)',
+    'rgb(61, 103, 216),rgb(65, 80, 170),rgb(53, 69, 115)',
+    'rgb(54, 181, 183),rgb(19, 126, 105)',
+  ];
+
+  return gradients[Math.floor(Math.random() * gradients.length)];
+};
