d3894da4e5beaef231268517b97f5a8564fbcc7c
jacando.tos <sereicikas.t@gmail.com>
2021-03-31 21:45:48 +0200

    finish card structure

--- src/components/List/Card/Card.tsx
+++ src/components/List/Card/Card.tsx
@@ -3,17 +3,24 @@ import { CardProps } from 'types/interfaces';
 import * as Styled from './styles';
 
 function Card(props: CardProps): JSX.Element {
-  const { firstName, lastName, email, picture, index, gradient } = props;
+  const { firstName, lastName, email, picture, index, gradient, username } = props;
   return (
     <Styled.Card delay={index}>
       <Styled.Background gradient={gradient}>
         <Styled.Picture src={picture} alt="User picture" />
       </Styled.Background>
       <Styled.Information>
-        <p>
+        <Styled.Name>
           {firstName} {lastName}
-        </p>
-        <h6>{email}</h6>
+        </Styled.Name>
+        <Styled.Row>
+          <h6>Username</h6>
+          <p>{username}</p>
+        </Styled.Row>
+        <Styled.Row>
+          <h6>Email</h6>
+          <p>{email}</p>
+        </Styled.Row>
       </Styled.Information>
     </Styled.Card>
   );
