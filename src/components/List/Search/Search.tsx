8449b178818d1a89125c6ad4952a93efcb2b9e01
jacando.tos <sereicikas.t@gmail.com>
2021-04-07 21:26:56 +0200

    add search component

--- src/components/List/Search/Search.tsx
+++ src/components/List/Search/Search.tsx
@@ -1,7 +1,19 @@
 import * as Styled from './styles';
 
-function Search(): JSX.Element {
-  return <Styled.Search type="text" placeholder="Search for..." />;
+import { SearchProps } from 'types/types';
+
+function Search({ value, setSearch, searchUser }: SearchProps): JSX.Element {
+  return (
+    <Styled.Search
+      value={value}
+      type="text"
+      placeholder="Search for..."
+      onChange={(e) => {
+        setSearch(e?.target?.value);
+        searchUser(e?.target?.value);
+      }}
+    />
+  );
 }
 
 export default Search;
