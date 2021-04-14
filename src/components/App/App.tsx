ddc049533fa1f5a05fb0fad75f61d17336e5da67
jacando.tos <sereicikas.t@gmail.com>
2021-04-14 13:23:27 +0200

    use theme in app component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,25 @@
-import { Fragment } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import List from '../List';
 import Settings from '../Settings';
+import { useTheme } from 'api/context/themeContext';
+import GlobalStyles from 'global-styles/styles';
+import { lightTheme, darkTheme } from 'global-styles/themes';
 
 function App(): JSX.Element {
+  const { theme } = useTheme();
+  const selectedTheme = theme === 'light' ? lightTheme : darkTheme;
+
   return (
-    <Fragment>
-      <Switch>
-        <Route component={List} exact path="/" />
-        <Route component={Settings} exact path="/settings" />
-      </Switch>
-    </Fragment>
+    <ThemeProvider theme={selectedTheme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Switch>
+          <Route component={List} exact path="/" />
+          <Route component={Settings} exact path="/settings" />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
